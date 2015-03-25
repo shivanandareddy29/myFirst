17d2884025e3920c24965aedd63eaadd0c471120
shivanandareddy29 <shivanandareddy29@gmail.com>
2015-03-25 10:35:01 -0500

    have modified the file

--- sum.c
+++ sum.c
@@ -3,7 +3,7 @@
 void main()
 {
 int i,value,sum=0;
-printf("\n enter a value to mind the sum of numbers ");
+printf("\n enter a value to find the sum of numbers: ");
 scanf("%d",&value);
 for (i=0;i<value;i++)
 {
